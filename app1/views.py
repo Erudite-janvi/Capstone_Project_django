c5ec65a2d068db3b53b3fac91019be0f6b168dbd
Janvi sharma <janvisharma@Janvis-MacBook-Pro.local>
2024-03-26 14:38:52 -0400

    update_user_details

--- app1/views.py
+++ app1/views.py
@@ -30,3 +30,18 @@ class UserListCreateAPIView(APIView):
         users = login.objects.all()
         serializer = LoginSerializer(users, many=True)
         return Response(serializer.data)
+    
+    def put(self, request, user_id=None):
+        if user_id is not None:
+            try:
+                user = login.objects.get(id=user_id)
+                serializer = LoginSerializer(user, data=request.data)
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response(serializer.data)
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            except login.DoesNotExist:
+                raise Http404("User does not exist")
+        else:
+            return Response({"error": "User ID not provided"}, status=status.HTTP_400_BAD_REQUEST)
+
